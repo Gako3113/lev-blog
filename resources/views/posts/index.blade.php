a49e618ed2aca92d5e056285e9259bb62b0f7ee1
Gako3113 <kougagaga@gmail.com>
2021-11-29 08:30:53 +0900

    ブログ投稿詳細の表示

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,21 +7,23 @@
         <title>Blog</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        //<link href="https:fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
     </head>
     <body>
         <h1>Blog name</h1>
-        <div class='posts'>
+        <div class="posts">
             @foreach ($posts as $post)
-                <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
-                    <p class='body'>{{$post->body}}</p>
+                <div class="post">
+                    <h2 class="title">
+                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                    </h2>
+                    <p class="body">{{$post->body}}</p>
                 </div>
             @endforeach
         </div>
-        <div class='paginate'>
+        <div class="paginate">
             {{ $posts->links() }}
         </div>
     </body>
