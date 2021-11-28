91eb80fc82ee925a7b72bcee2da6b1e3d868765a
Gako3113 <kougagaga@gmail.com>
2021-11-29 00:21:40 +0900

    postの実装２

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,7 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return $post->get();
+        return view('posts/index')->with(['posts'=>$post->getByLimit()]); 
+        //ViewにController内で取得した変数を渡したい場合は、Viewインスタンスに対してwithというメソッドを使用する
     }
 }
