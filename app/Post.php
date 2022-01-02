23883a21a12d0d4c280dd9d6a9a5e63d80a7d40a
Gako3113 <kougagaga@gmail.com>
2022-01-03 00:57:18 +0900

    リレーションの練習

--- app/Post.php
+++ app/Post.php
@@ -9,14 +9,24 @@ class Post extends Model
 {
     use SoftDeletes;
     
-    public function getPaginateByLimit(int $limit_count = 10)
+    //モデル :: with(リレーション名) -> paginate()またはget()など; = Eagerローディングといい、リレーションによって増えてしまうデータベースアクセスの回数を減らす　重くならないようにする 
+    public function getPaginateByLimit(int $limit_count = 5)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける。DESC＝降順、ASC＝昇順
-        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        return $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
+    
     protected $fillable = [
             'title',
             'body',
+            'category_id'
         ];
+    
+    //Categoryに対するリレーション
+    //Postモデルのインスタンスから関連する1つのCategoryモデルのインスタンス->単数形categoryを呼び出せる
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
 
